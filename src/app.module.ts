77a6f33b46245e299f6655c5a36fd674481d2de7
Anthony Guevara <yalidanthony@gmail.com>
2022-10-23 11:31:13 -0600

    Schema validation for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { enviroments } from './enviroments';
 import config from './config';
 
@@ -21,6 +23,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
