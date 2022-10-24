b0bf35c265655828604efb444506f1f14b6380b6
Anthony Guevara <yalidanthony@gmail.com>
2022-10-23 20:50:37 -0600

    Comentarios

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  app.enableCors();
+  app.enableCors(); //habilita apertura a cualquir dominio
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
